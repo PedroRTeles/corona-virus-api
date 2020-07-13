06bcca1fbe9c83548a3e35c9419870ce204b2eb4
Pedro Teles <pedroateles@protonmail.com>
2020-07-13 14:27:57 -0300

    initial commit

---
+++ src/main/java/dev/pedroteles/covid/starter/CoronaVirusApiApplication.java
@@ -0,0 +1,13 @@
+package dev.pedroteles.covid.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaVirusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaVirusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pedroteles/covid/CoronaVirusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pedroteles.covid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaVirusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
